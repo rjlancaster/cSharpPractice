7692a62827ae4d7bbde0c36a4243515655e14339
rjlancaster <rjlancaster@gmail.com>
2019-04-07 18:10:54 -0500

    more exercises

--- fitnessFrog/Program.cs
+++ fitnessFrog/Program.cs
@@ -7,17 +7,31 @@ namespace Treehouse.FitnessFrog
         static void Main()
         {
             int runningTotal = 0;
-            //Prompt the user for minutes exercised
-            Console.Write("Enter how many minutes you exercised: ");
-            string entry = Console.ReadLine();
-            int minutes = int.Parse(entry);
-            runningTotal = runningTotal + minutes;
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                //Prompt the user for minutes exercised
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+                string entry = Console.ReadLine();
+
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    int minutes = int.Parse(entry);
+                    runningTotal = runningTotal + minutes;
+                }
+            }
             //Add minutes exercised to total
 
 
             //Display total minutes exercised to the screen
             Console.WriteLine("You've exercised " + runningTotal + " minutes.");
             //Repeat until the user quits
+
+            Console.WriteLine("bye!");
         }
     }
 }
