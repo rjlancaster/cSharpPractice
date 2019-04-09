cef5cbedc0fa18df6d0f16a5855d824d4a277059
rjlancaster <rjlancaster@gmail.com>
2019-04-09 16:09:21 -0500

    continuing c# objects course

--- TowerDefense/Game.cs
+++ TowerDefense/Game.cs
@@ -1,15 +1,24 @@
+using System;
+
 namespace TreehouseDefense
 {
     class Game
     {
         public static void Main()
         {
-            Tower tower = new Tower();
-            Map map = new Map();
-            map.Width = 8;
-            map.Height = 5;
+            //Tower tower = new Tower();
+            Map map = new Map(8, 5);
 
-            int area = map.Width * map.Height;
+            Point point = new Point(4, 2);
+            //bool isOnMap = map.OnMap(point);
+            //Console.WriteLine(isOnMap);
+
+            //point = new Point(8, 5);
+            //isOnMap = map.OnMap(point);
+            //Console.WriteLine(isOnMap);
+
+            Console.WriteLine(point.DistanceTo(5, 5));
+
         }
     }
 }
--- TowerDefense/Map.cs
+++ TowerDefense/Map.cs
@@ -2,7 +2,18 @@ namespace TreehouseDefense
 {
     class Map
     {
-            public int Width;
-            public int Height;
+            public readonly int Width;
+            public readonly int Height;
+
+            public Map(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public bool OnMap(Point point)
+            {
+                return point.X >= 0 && point.X < Width && point.Y >= 0 && point.Y < Height;
+            }
     }
 }
