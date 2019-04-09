01f94fa0a78bde1d94aa41545a9818e01229e1d8
rjlancaster <rjlancaster@gmail.com>
2019-04-09 14:28:05 -0500

    began C# objects course

--- fitnessFrog/Program.cs
+++ fitnessFrog/Program.cs
@@ -65,35 +65,3 @@ namespace Treehouse.FitnessFrog
         }
     }
 }
-
-
-using System;
-
-namespace Treehouse.CodeChallenges
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.Write("Enter the number of times to print \"Yay!\": ");
-            string input = Console.ReadLine();
-
-            try
-            {
-                int count = int.Parse(input);
-
-                int i = 0;
-                while(i < count)
-                {
-                    i += 1;
-                    Console.WriteLine("Yay!");
-                }
-            }
-            catch(FormatException)
-            {
-            	Console.WriteLine("You must enter a whole number.");
-            }
-
-        }
-    }
-}
